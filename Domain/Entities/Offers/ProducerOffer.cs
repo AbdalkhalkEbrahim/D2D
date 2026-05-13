ece1a02f67590034a0a327a4bfcde01af45269a5
2llaa <alaay1604@gmail.com>
2026-05-13 18:29:22 +0300

    hhhhhhh

--- Domain/Entities/Offers/ProducerOffer.cs
+++ Domain/Entities/Offers/ProducerOffer.cs
@@ -1,14 +1,9 @@
 ﻿using Domain.Entities.Shared;
 using Domain.Enums;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Domain.Entities.Offers
 {
-    public class ProducerOffer:Audits
+    public class ProducerOffer : Audits
     {
         public int ID { get; set; }
         public decimal Price { get; set; }
