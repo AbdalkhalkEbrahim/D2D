359f3fac93ab032c5ab4a64455739aed40d766c0
Abdalkhalk Ebrahim <abdalkhalkebrahim2411@gmail.com>
2026-07-01 20:00:13 +0300

    jsd

--- Application/Services/AuthService.cs
+++ Application/Services/AuthService.cs
@@ -136,7 +136,7 @@ namespace Application.Services
             };
             _context.Update(counters);
             await _context.SaveChangesAsync();
-            AnonCounter++;
+=            string leadingZeros = new string('0', 6 - AnonCounter.ToString().Length);
             string leadingZeros = new string('0', 6 - AnonCounter.ToString().Length);
             return $"Anon{leadingZeros}{AnonCounter}_{userType.ToString()}";
         }
