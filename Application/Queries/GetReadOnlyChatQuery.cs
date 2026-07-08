ab64535e17e0381243084c91d03dc8b8a950eff6
2llaa <alaay1604@gmail.com>
2026-07-08 20:25:15 +0300

    restoring all endpoints

--- Application/Queries/GetReadOnlyChatQuery.cs
+++ Application/Queries/GetReadOnlyChatQuery.cs
@@ -1,11 +1,6 @@
 ﻿using Application.Response;
 using Domain.DTOs.Admin;
 using MediatR;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Application.Queries
 {
