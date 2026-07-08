f3cad72ce6cc841cbe668b2f3306a981109eb62c
Abdalkhalk Ebrahim <abdalkhalkebrahim2411@gmail.com>
2026-07-08 13:27:23 +0300

    get all producer offers + get all chat messages

---
+++ Application/Handlers/AccountSettingsFeature/AddBalanceCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using Application.Commands.AccountSettingsFeature;
+using Application.Response;
+using Infrastructure.Data.Context;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Handlers.AccountSettingsFeature
+{
+    public class AddBalanceCommandHandler : IRequestHandler<AddBalanceCommand, Result<int>>
+    {
+        public AddBalanceCommandHandler(D2DContext context)
+        {
+            
+        }
+        public Task<Result<int>> Handle(AddBalanceCommand request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Application/Queries/OffersFeature/GetAllProducerOffersQuery.cs
@@ -0,0 +1,23 @@
+﻿using Application.Response;
+using Domain.DTOs.OfferDtos;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Queries.OffersFeature
+{
+    public class GetAllProducerOffersQuery:IRequest<Result<List<ProducerOfferResponse>>>
+    {
+
+        public string ProducerId { get; set; }
+        public bool Newest { get; set; } = true;
+        public bool IsAccepted { get; set; }
+        public bool IsPending { get; set; }
+        public bool IsDecilned { get; set; }
+        public int? PageNum { get; set; } = 1;
+        public int? PageSize { get; set; } = 4;
+    }
+}
