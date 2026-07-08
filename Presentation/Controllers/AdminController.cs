d8f4c02408a48f6a286b840c2e43551e231f7ca3
2llaa <alaay1604@gmail.com>
2026-07-08 04:05:38 +0300

    solve readonly chat

--- Presentation/Controllers/AdminController.cs
+++ Presentation/Controllers/AdminController.cs
@@ -1,5 +1,7 @@
 ﻿using Application.Commands.Admin;
+using Application.Queries;
 using Application.Queries.Admin;
+using Domain.DTOs;
 using Domain.DTOs.AccountSettingsDtos;
 using Domain.DTOs.Admin;
 using MediatR;
